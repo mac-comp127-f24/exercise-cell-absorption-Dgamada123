224b6d688c8a0b79ca2ab799f65448be33d9d7f4
Dureti Gamada <dgamada@macalester.edu>
2024-10-02 19:17:36 -0500

    JustBeginningCellSimulation

--- src/cellabsorption/Cell.java
+++ src/cellabsorption/Cell.java
@@ -1,55 +1,24 @@
 package cellabsorption;
 
-import edu.macalester.graphics.CanvasWindow;
+
+
 import edu.macalester.graphics.Ellipse;
 import edu.macalester.graphics.Point;
-
 import java.awt.Color;
-import java.util.Random;
 
-@SuppressWarnings("SameParameterValue")
-public class CellSimulation {
+
+public class Cell {
     private static final double
         WIGGLINESS = 0.2,
         WANDER_FROM_CENTER = 60000;
+    
 
-    private CanvasWindow canvas;
-    private Random rand = new Random();
     private Ellipse shape;
     private double radius;
     private double direction;
-    private Cell cell;
 
-    public static void main(String[] args) {
-        new CellSimulation();
-    }
 
-    public CellSimulation() {
-        canvas = new CanvasWindow("Cell Absorption", 800, 800);
-        populateCells();
-
-        //noinspection InfiniteLoopStatement
-        while (true) {
-            Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
-            moveAround(canvasCenter);
-            grow(0.02);
-
-            canvas.draw();
-            canvas.pause(10);
-        }
-    }
-
-    private void populateCells() {
-        double size = rand.nextInt(5) + 2;
-        createCell(
-            rand.nextDouble() * (canvas.getWidth() - size),
-            rand.nextDouble() * (canvas.getWidth() - size),
-            size,
-            Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
-        canvas.add(shape);
-    }
-
-    private void createCell(double x, double y, double radius, Color color) {
+    public Cell(double x, double y, double radius, Color color) {
         shape = new Ellipse(x, y, radius * 2, radius * 2);
         shape.setFillColor(color);
         this.radius = radius;
@@ -91,4 +60,9 @@ public class CellSimulation {
         double pi2 = Math.PI * 2;
         return ((theta + Math.PI) % pi2 + pi2) % pi2 - Math.PI;
     }
+
+
+
+
+
 }
